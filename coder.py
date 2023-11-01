e84ab78db13bacba36ca297f9da75519e615dc44
Alina_Orlova <orlova.aa@phystech.edu>
2023-11-01 21:46:28 +0300

    New files

---
+++ coder.py
@@ -0,0 +1,35 @@
+def scheme_to_json():
+    """
+    Coding scheme to json-file according to special format
+    :param main: entity of scheme class Main
+    :param filename: name of json file to save
+    :return:
+    """
+    pass
+
+
+def code_primitive():
+    """
+    Coding primitive to json-object according to special format
+    :param primitive: entity of class Primitive
+    :return: json-object
+    """
+    pass
+
+
+def code_block():
+    """
+    Coding block to json-object according to special format
+    :param block: entity of class Block
+    :return: json-object
+    """
+    pass
+
+
+def code_main():
+    """
+    Coding main scheme to json-object according to special format
+    :param main: entity of schema class Main
+    :return: json-object
+    """
+    pass
---
+++ parser.py
@@ -0,0 +1,34 @@
+def json_to_scheme():
+    """
+    Parsing scheme from json-file
+    :param filename: name of .json file
+    :return: entity of class Main
+    """
+    pass
+
+
+def parse_primitive():
+    """
+    Parsing primitive from json-object
+    :param json_obj: json-object
+    :return: entity of class Primitive
+    """
+    pass
+
+
+def parse_block():
+    """
+    Parsing block from json-object
+    :param json_obj: json-object
+    :return: entity of class Block
+    """
+    pass
+
+
+def parse_main():
+    """
+    Parsing main scheme from json-object
+    :param json_obj: json-object
+    :return: entity of schema class Main
+    """
+    pass
