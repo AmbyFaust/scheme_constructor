70da7f1b54c1463f4b699f0e3412367b9778b3ee
Alina_Orlova <orlova.aa@phystech.edu>
2023-11-01 23:58:11 +0300

    New_coder

--- coder.py
+++ coder.py
@@ -1,35 +1,87 @@
-def scheme_to_json():
+import schema_classes
+
+def converting_to_json(dict):
+    """
+    Coding dictionary to json-file
+    :param dict: dictionary with entities
+    return json
+    """
+    pass
+
+def code_pins(pins):
+    """
+    Coding pins to list of json objects
+    :param pins: entities of class Pin
+    :return: list of json objects
+    """
+    list_of_pins = []
+    for pin in pins:
+        pin_dictionary = {
+            'name' = pin.get_name(),
+            'top_left' = pin.get_top_left()
+        }
+        list_of_pins.append(converting_to_json(pin_dictionary))
+    return list_of_pins
+
+
+def code_pin_nets(pin_nets):
+    """
+    Coding main scheme to json-object according to special format
+    :param pin_net: entity of Pin_connections
+    :return: list of json objects
+
+    """
+    list_of_pin_nets = []
+    for pn in pin_nets:
+        pin_nets_dictionary = {
+            'name' = pn.get_name(),
+        'pins' = code_pins(pn.get_pins()),
+        'lines' = pn.get_lines()  # Надо придумать в каком виде это записывать
+        }
+        list_of_pin_nets.append(converting_to_json(pin_nets_dictionary))
+    return list_of_pin_nets
+
+
+def code_objects(objects):
+    """
+    Coding objects to list of json objects
+    :param list of objects: entities of class Primitive and Block
+    :return: list of json objects
+    """
+    list_of_objects = []
+    for obj in objects:
+        obj_dictionary = {
+            'type' = obj.get_type(),
+            'name' = obj.get_name(),
+            'top_left' = obj.get_top_left(),
+            'width' = obj.get_width(),
+            'height' = obj.get_height(),
+            'pins' = code_pins(obj.get_pins())
+        }
+        list_of_objects.append(converting_to_json(obj_dictionary))
+    return list_of_objects
+
+
+def scheme_to_json(schema, filename):
     """
     Coding scheme to json-file according to special format
-    :param main: entity of scheme class Main
+    :param scheme: dictionary with objects
     :param filename: name of json file to save
     :return:
     """
-    pass
+    list_of_blocks = []  # лист в который будут складываться блоки в формате json
+    for obj in schema:
+        obj_description = {
+            'type' = obj.get_type(),
+        'name' = obj.get_name(),
+        'top_left' = obj.get_top_left(),
+        'width' = obj.get_width(),
+        'height' = obj.get_height(),
+        'objects' = code_objects(obj.get_objects()),
+        'pins' = code_pins(obj.get_pins()),
+        'pin_nets' = code_pin_nets(obj.get_pin_nets())
+        }
+        list_of_blocks.append(converting_to_json(obj_description))
 
-
-def code_primitive():
-    """
-    Coding primitive to json-object according to special format
-    :param primitive: entity of class Primitive
-    :return: json-object
-    """
-    pass
-
-
-def code_block():
-    """
-    Coding block to json-object according to special format
-    :param block: entity of class Block
-    :return: json-object
-    """
-    pass
-
-
-def code_main():
-    """
-    Coding main scheme to json-object according to special format
-    :param main: entity of schema class Main
-    :return: json-object
-    """
+    # Здесь будет код записи списка json объектов в файл
     pass
