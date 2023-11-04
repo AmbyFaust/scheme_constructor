e8ee013b6d1edc05dae0f1ec4df6d4dacfe99559
Orlova Alina <71490951+Rabotah@users.noreply.github.com>
2023-11-04 19:16:05 +0300

    Add files via upload

---
+++ parser.py
@@ -0,0 +1,15 @@
+def json_to_schema(json):
+    pass
+
+def window_with_error():
+    pass
+
+def convert_pins(list_of_pins):
+    pass
+
+def convert_connections(list_of_connections):
+    pass
+
+def convert_blocks(list_of_blocks):
+    pass
+
