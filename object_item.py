d1dc3216155f917833cfc5aeee9e312a74310751
Bogdan <38697168+Tokomeir@users.noreply.github.com>
2023-11-06 02:26:37 +0300

    Uploaded partial realisation of ObjectPanel
    lul

---
+++ object_item.py
@@ -0,0 +1,61 @@
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import QRect, QSize, QPoint
+
+from PyQt5.QtWidgets import QListWidgetItem, QWidget
+from PyQt5.QtGui import QIcon
+from gui.rendering_widget import Primitive, PrimitiveWidget, Block, BlockWidget, PinWidget
+from settings import pin_width, pin_height
+
+from copy import deepcopy
+
+
+class ObjectItem(QListWidgetItem):
+    def __init__(self, text:str, obj:QWidget = None):
+        super(ObjectItem, self).__init__(text)
+
+        self.__data = None
+        if obj:
+            self.setStoredObject(obj)
+
+    def setStoredObject(self, obj: QWidget):
+        self.__data = _ObjectDublicator.clone(obj)
+        padding = min(pin_width, pin_height)
+        self.setIcon(QIcon(self.__data.grab(QRect(QPoint(-padding//2,-padding//2), 
+                                            QSize(self.__data.width() + padding, self.__data.height() + padding)))))
+
+    def getStoredObject(self) -> QWidget:
+        return _ObjectDublicator.clone(self.__data)
+
+# создаЄт начальный примитив, добавл€емый в список при инициализации
+def createNewPrimitiveObjectItem():
+        new_primitive = Primitive("new", [], 0, 0, 100, 50)
+        new_primitive_widget = PrimitiveWidget(None, controller=None, primitive=new_primitive)
+        return ObjectItem("", new_primitive_widget)
+
+
+#    singledispatchmethod отказываетс€ работать, поэтому статик
+#    вспомогательный класс клонировани€ виджетов
+#
+#    !! ” созданного клона родитель объекта устанавливаетс€ None !!
+#    !! –одителем всех пинов €вл€етс€ клон                       !!
+#    !!  оординаты пинов задаютс€ в локальной системе координат  !!
+#    !! Ёто важно учитывать при получении пинов                  !!
+class _ObjectDublicator:
+    @staticmethod
+    def clone(obj):
+        cloned = None
+        if isinstance(obj, PrimitiveWidget):
+            cloned = PrimitiveWidget(None, deepcopy(obj.primitive), obj.controller)
+
+        elif isinstance(obj, BlockWidget):
+            cloned = BlockWidget(None, deepcopy(obj.block), obj.controller)
+
+        if not cloned:
+            return cloned
+
+        for pin in obj.pin_widgets:
+            pin_widget = PinWidget(cloned, cloned)
+            pin_widget.move(pin.pos() - obj.pos())
+            cloned.pin_widgets.append(pin_widget)
+            pin_widget.show()
+        return cloned
