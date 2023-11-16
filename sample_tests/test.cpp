4343ec671f46a881c14895f85ee4485f251199de
iluhinoBruho <61965482+iluhinoBruho@users.noreply.github.com>
2023-11-16 23:39:50 +0300

    check_tools and graph tests - 11 tests total

--- sample_tests/test.cpp
+++ sample_tests/test.cpp
@@ -12,13 +12,13 @@
 #include "../Checker/graph.cpp"
 
 
+
 TEST(Example, ExampleTestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
 
 TEST(GraphTest, CycledGraph) {
-    // Âàøè òåñòîâûå ïðîâåðêè çäåñü
     Graph g;
     for(int i = 0; i < 5; i++)
         g.add_vertex();
@@ -44,6 +44,101 @@ TEST(GraphTest, ThreeComponents) {
     g.add_vertex();
     EXPECT_EQ(g.get_components_cnt(), 3);
 }
+
+TEST(GraphTest, NComponents) {
+    Graph g;
+    int components = 100000; //1000000;
+    for (int i = 1; i < components * 2; i += 2) {
+        g.add_vertex();
+        g.add_vertex();
+        g.add_edge(i, i - 1);
+    }
+    EXPECT_EQ(g.get_components_cnt(), components);
+}
+
+TEST(CheckToolsTest, UpperHierrachyCycle) {
+    Graph g;
+    int vertexes = 100;
+    g.add_vertex();
+    for (int i = 1; i < vertexes; i++) {
+        g.add_vertex();
+        g.add_edge(i, i - 1);
+    }
+    g.add_edge(vertexes - 1, 0);
+    EXPECT_EQ(g.is_cycled(), true);
+    EXPECT_EQ(upper_hierarchy_check(g), true);
+}
+
+TEST(CheckToolsTest, UpperHierrachyNotCycle) {
+    Graph g;
+    int vertexes = 100;
+    g.add_vertex();
+    for (int i = 1; i < vertexes; i++) {
+        g.add_vertex();
+        if(i % 2)
+            g.add_edge(i, i - 1);
+    }
+    g.add_edge(vertexes - 1, 0);
+    EXPECT_EQ(g.is_cycled(), false);
+    EXPECT_EQ(upper_hierarchy_check(g), false);
+}
+
+TEST(CheckToolsTest, LowerHierrachyConnectedCircuit) {
+    Graph g;
+    int vertexes = 100;
+    g.add_vertex();
+    for (int i = 1; i < vertexes; i++) {
+        g.add_vertex();
+        g.add_edge(i, 0);
+    }
+    EXPECT_EQ(g.get_components_cnt() == 1, true);
+    EXPECT_EQ(unconnected_circuit_check(g), false);
+}
+
+
+TEST(CheckToolsTest, LowerHierrachyCorrect) {
+    Graph g;
+    int vertexes = 100;
+    g.add_vertex();
+    for (int i = 1; i < vertexes; i++) {
+        g.add_vertex();
+        if (i % 2)
+            g.add_edge(i, i - 1);
+    }
+    g.add_edge(vertexes - 1, 0);
+    EXPECT_EQ(g.is_cycled(), false);
+    EXPECT_EQ(upper_hierarchy_check(g), false);
+}
+
+
+TEST(CheckToolsTest, LowerHierrachyUnconnectedCircuit) {
+    Graph g;
+    int vertexes = 100;
+    g.add_vertex();
+    for (int i = 1; i < vertexes; i++) {
+        g.add_vertex();
+        if (i % 3)
+            g.add_edge(i, i - 1);
+    }
+    EXPECT_EQ(g.get_components_cnt() > 1, true);
+    EXPECT_EQ(unconnected_circuit_check(g), true);
+}
+
+
+TEST(CheckToolsTest, LowerHierrachyUnconnectedCircuitNoneEdges) {
+    Graph g;
+    int vertexes = 1000;
+    g.add_vertex();
+    for (int i = 1; i < vertexes; i++) {
+        g.add_vertex();
+        g.add_edge(i, i - 1);
+    }
+    g.add_edge(vertexes-1, 0);
+    EXPECT_EQ(empty_pin_check(g), false);
+    EXPECT_EQ(unconnected_circuit_check(g), false);
+    EXPECT_EQ(lower_hierarchy_check(g), false);
+}
+
 
 
 int main(int argc, char** argv) {
