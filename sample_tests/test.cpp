5a4443e49b826f99e4b34c8f48c104b3d66ed23e
iluhinoBruho <61965482+iluhinoBruho@users.noreply.github.com>
2023-11-16 22:27:48 +0300

    updated include folders in test.cpp

--- sample_tests/test.cpp
+++ sample_tests/test.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 
 
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\check_tools.h"
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\netlist.h"
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\parse_tools.h"
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\graph.h"
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\check_tools.cpp"
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\netlist.cpp"
-//#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\parse_tools.cpp"
-#include "\Users\L-408-21\Desktop\MIPT\Cpract\Checker\Checker\graph.cpp"
+#include "../Checker/check_tools.h"
+#include "../Checker/netlist.h"
+#include "../Checker/parse_tools.h"
+#include "../Checker/graph.h"
+#include "../Checker/check_tools.cpp"
+#include "../Checker/netlist.cpp"
+//#include "../Checker/parse_tools.cpp"
+#include "../Checker/graph.cpp"
+
 
 TEST(Example, ExampleTestName) {
   EXPECT_EQ(1, 1);
@@ -17,7 +18,7 @@ TEST(Example, ExampleTestName) {
 }
 
 TEST(GraphTest, CycledGraph) {
-    // Ваши тестовые проверки здесь
+    // Г‚Г ГёГЁ ГІГҐГ±ГІГ®ГўГ»ГҐ ГЇГ°Г®ГўГҐГ°ГЄГЁ Г§Г¤ГҐГ±Гј
     Graph g;
     for(int i = 0; i < 5; i++)
         g.add_vertex();
@@ -28,7 +29,6 @@ TEST(GraphTest, CycledGraph) {
 }
 
 TEST(GraphTest, TwoComponents) {
-    // Ваши тестовые проверки здесь
     Graph g;
     g.add_vertex();
     g.add_vertex();
@@ -37,9 +37,7 @@ TEST(GraphTest, TwoComponents) {
     EXPECT_EQ(g.get_components_cnt(), 2);
 }
 
-
 TEST(GraphTest, ThreeComponents) {
-    // Ваши тестовые проверки здесь
     Graph g;
     g.add_vertex();
     g.add_vertex();
