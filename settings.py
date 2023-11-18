561341a29b0652890504dbdff28196bce270c18e
Bogdan <bogdan.allaberdin2@gmail.com>
2023-11-19 02:33:08 +0300

    подготовка к тестам

---
+++ settings.py
@@ -0,0 +1,16 @@
+rendering_widget_width = 400
+rendering_widget_height = 400
+
+primitive_width = 100
+primitive_height = 50
+
+block_width = primitive_width
+block_height = primitive_height
+
+pin_width = 16
+pin_height = 16
+
+crossroad_width = 10
+crossroad_height = 10
+
+width_wire = 15  # должно быть нечетным
