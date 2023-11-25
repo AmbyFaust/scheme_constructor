4e575b55105945387a069f7a3e58021799eda032
Jashugan <48289990+Jashu9an@users.noreply.github.com>
2023-11-25 15:21:10 +0300

    Update netlist.h

--- Checker/netlist.h
+++ Checker/netlist.h
@@ -23,10 +23,16 @@ public:
 
 class Primitive : public Object
 {
+protected:
+    std::string name;
+    std::vector<std::string> pins;
 public:
     Primitive() {}
-    Primitive(std::vector<int> init_pins) { pins = init_pins; }
+    Primitive(std::string n, std::vector<std::string> vec_pins);
+    Primitive(std::vector<int> init_pins);
     ~Primitive() = default;
+
+    void show();
 
     block_type type() const override { return primitive;  };
 };
@@ -35,20 +41,33 @@ public:
 
 class NetList : public Object
 {
+protected:
+    // for testing
+    std::vector<int> pins_int;
+
+    std::string name;                                           // name of block
+    std::vector<std::string> pins;                              // list of all pins
+    std::vector<std::string> edge_pins;                         // list of pins which are located on edge of block
+    std::map<std::string, std::vector<std::string>> pin_nets;   // list of pins_connections
+
+    // inner elements
+    std::vector<Object*> objects;
+    // inner connections
+    std::vector<std::set<int>> connections;
+
 public:
     NetList() {}
-    NetList(std::vector<int> init_pins) { pins = init_pins; }
+    NetList(std::string n, std::vector <std::string> e_pins, std::vector<std::string> pins, std::map<std::string, std::vector<std::string>> m_pin_nets);
+    NetList(std::string n, std::vector<Object> objects, std::vector <std::string> e_pins, std::vector<std::string> pins, std::map<std::string, std::vector<std::string>> m_pin_nets);
+    NetList(std::vector<int> init_pins);
     ~NetList() = default;
 
     block_type type() const override { return netlist; };
     void add_connection(std::set<int> connection);
 
+    void show();                                                 // demonstration of class'es info
+
     const std::vector<std::set<int>>& get_connections() const { return connections; }
-private:
-    // inner elements
-    std::vector<Object*> elements;
-    // inner connections
-    std::vector<std::set<int>> connections;
 };
 
 
